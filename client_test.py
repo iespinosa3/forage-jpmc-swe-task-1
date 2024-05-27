c1dbe829db8d5f43cfa0bb0fc6e586646075224a
iespinosa <espi_nosa3@yahoo.com>
2024-05-27 04:27:15 -0400

    create assertion

--- client_test.py
+++ client_test.py
@@ -8,6 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      expected_price = (bid_price + ask_price) / 2
+
+    self.assertEqual(stock, quote['stock'])
+    self.assertEqual(bid_price, quote['top_bid']['price'])
+    self.assertEqual(ask_price, quote['top_ask']['price'])
+    self.assertEqual(price, expected_price)
+    
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +24,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      
+      self.assertGreater(bid_price, ask_price)
 
   """ ------------ Add more unit tests ------------ """
 
